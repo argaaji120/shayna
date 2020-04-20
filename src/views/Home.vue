b72f8001a7bb8db7133299fb9081c75371dee8b1
argaaji120 <argaaji120@gmail.com>
2020-04-20 21:30:49 +0700

    Upload project

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <BannerShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import BannerShayna from "@/components/BannerShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    BannerShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+};
+</script>
+
+<style scoped>
+.carousel-bg {
+  background-image: url("/assets/img/hero-1.jpg");
+}
+</style>
---
+++ src/views/Product.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="product">
+    <HeaderShayna />
+
+    <!-- Breadcrumb Section Begin -->
+    <div class="breadcrumb-section text-left">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="breadcrumb-text product-more">
+              <router-link to="/">
+                <i class="fa fa-home"></i>Home
+              </router-link>
+              <span>Detail</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Breadcrumb Section Begin -->
+
+    <!-- Product Shop Section Begin -->
+    <section class="product-shop spad page-details">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="product-pic-zoom">
+                  <img class="product-big-img" :src="image_default" />
+                </div>
+                <div class="product-thumbs">
+                  <carousel
+                    :nav="false"
+                    :dots="false"
+                    :items="3"
+                    class="product-thumbs-track ps-slider"
+                  >
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[0])"
+                      :class="thumbs[0] == image_default ? 'active' : '' "
+                    >
+                      <img src="assets/img/mickey1.jpg" />
+                    </div>
+
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[1])"
+                      :class="thumbs[1] == image_default ? 'active' : '' "
+                    >
+                      <img src="assets/img/mickey2.jpg" />
+                    </div>
+
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[2])"
+                      :class="thumbs[2] == image_default ? 'active' : '' "
+                    >
+                      <img src="assets/img/mickey3.jpg" />
+                    </div>
+
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[3])"
+                      :class="thumbs[3] == image_default ? 'active' : '' "
+                    >
+                      <img src="assets/img/mickey4.jpg" />
+                    </div>
+                  </carousel>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="product-details text-left">
+                  <div class="pd-title">
+                    <span>oranges</span>
+                    <h3>Pure Pineapple</h3>
+                  </div>
+                  <div class="pd-desc">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.</p>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus quisquam animi, commodi, nihil voluptate nostrum neque architecto illo officiis doloremque et corrupti cupiditate voluptatibus error illum. Commodi expedita animi nulla aspernatur.
+                      Id asperiores blanditiis, omnis repudiandae iste inventore cum, quam sint molestiae accusamus voluptates ex tempora illum sit perspiciatis. Nostrum dolor tenetur amet, illo natus magni veniam quia sit nihil dolores.
+                      Commodi ratione distinctio harum voluptatum velit facilis voluptas animi non laudantium, id dolorem atque perferendis enim ducimus? A exercitationem recusandae aliquam quod. Itaque inventore obcaecati, unde quam
+                      impedit praesentium veritatis quis beatae ea atque perferendis voluptates velit architecto?
+                    </p>
+                    <h4>$495.00</h4>
+                  </div>
+                  <div class="quantity">
+                    <router-link to="shopping-cart" class="primary-btn pd-cart">Add To Cart</router-link>
+                    <!-- <a href="shopping-cart.html" class="primary-btn pd-cart">Add To Cart</a> -->
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Product Shop Section End -->
+
+    <RelatedProductShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+import RelatedProductShayna from "@/components/RelatedProductShayna.vue";
+
+import carousel from "vue-owl-carousel";
+
+export default {
+  name: "Product",
+  components: {
+    HeaderShayna,
+    FooterShayna,
+    RelatedProductShayna,
+    carousel
+  },
+  data() {
+    return {
+      image_default: "assets/img/mickey1.jpg",
+      thumbs: [
+        "assets/img/mickey1.jpg",
+        "assets/img/mickey2.jpg",
+        "assets/img/mickey3.jpg",
+        "assets/img/mickey4.jpg"
+      ]
+    };
+  },
+  methods: {
+    changeImage(urlImage) {
+      this.image_default = urlImage;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.product-thumbs .pt {
+  margin-right: 10px;
+}
+</style>
